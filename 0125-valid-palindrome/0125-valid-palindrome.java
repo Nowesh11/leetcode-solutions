e92c09de96842d7d372ec58296b612ad56f5092d
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-04-29 14:45:25 +0800

    Time: 13 ms (35.84%), Space: 46.3 MB (31.25%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        String res = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        
+
+        int left = 0;
+        int right= res.length()-1;
+        
+
+        while(left<=right){
+
+            if(res.charAt(left)!=res.charAt(right))return false;
+            left++;
+            right--;
+
+
+
+            
+        }
+        return true;
+
+        
+    }
+    
+}
