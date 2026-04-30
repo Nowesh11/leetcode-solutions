629620b7d81ae98f6f0ebed63bbf24c4e24bb065
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-04-30 15:14:57 +0800

    Time: 602 ms (37.1%), Space: 213.9 MB (51.19%) - LeetHub

--- 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.java
+++ 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.java
@@ -1,36 +1,24 @@
 class WordFilter {
-    Map<String,Integer> map = new HashMap<>();
-    
+    HashMap<String, Integer> map = new HashMap<>();
 
     public WordFilter(String[] words) {
-        
-        int n = words.length;
+        for(int i = 0; i < words.length; i++){
+            String w = words[i];
+            int n = w.length();
 
-        for(int i =0;i<n;i++){
-            int m =words[i].length();
-
-            for(int p=1;p<=m;p++){
-                for(int s=1;s<=m;s++){
-
-                    String key =words[i].substring(0,p)+ "#" +words[i].substring(m-s);
-                    map.put(key,i);
+            // generate ALL prefix+suffix combinations
+            for(int p = 1; p <= n; p++){
+                for(int s = 1; s <= n; s++){
+                    String key = w.substring(0, p)  // prefix
+                               + "#"                // separator
+                               + w.substring(n - s);// suffix
+                    map.put(key, i); // overwrite = largest index
                 }
             }
         }
-
-        
-
-        
     }
-    
+
     public int f(String pref, String suff) {
-        return map.getOrDefault(pref + "#" + suff,-1);
-        
+        return map.getOrDefault(pref + "#" + suff, -1);
     }
 }
-
-/**
- * Your WordFilter object will be instantiated and called as such:
- * WordFilter obj = new WordFilter(words);
- * int param_1 = obj.f(pref,suff);
- */
