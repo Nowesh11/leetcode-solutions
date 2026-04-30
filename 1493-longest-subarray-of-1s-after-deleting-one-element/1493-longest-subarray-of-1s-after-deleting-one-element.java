345d49cdedcdb99470846e74da20de0d0fc343e1
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-04-30 16:18:05 +0800

    Time: 32 ms (4.13%), Space: 69.9 MB (5.38%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int mostFreq=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        int maxL=Integer.MIN_VALUE;
+        int left=0;
+
+        for(int right=0;right<nums.length;right++){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            mostFreq=map.getOrDefault(0,0);
+
+            while(mostFreq > 1){
+                map.put(nums[left],map.get(nums[left])-1);
+                if(nums[left]==0)mostFreq--;
+                left++;
+
+            }
+            maxL=Math.max(maxL,right-left);
+        }
+        return maxL;
+        
+    }
+}
