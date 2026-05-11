f0d344913764aa5b20370fc1a9560f757a22f0af
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-05-11 23:49:33 +0800

    Time: 20 ms (32.98%), Space: 82.5 MB (81.41%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int low = getMin(bloomDay);
+        int high = getMax(bloomDay);
+
+        if((long)m*k > bloomDay.length)return -1;
+        int ans=0;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(makeBouquet(bloomDay,m,k,mid)){
+                ans=mid;
+                high=mid -1;
+            }else{
+                low=mid+1;
+            }
+
+        }
+        return ans;
+        
+    }
+    public boolean makeBouquet(int[] arr,int bouquet,int flower,int mid){
+        int bouquetCount=0;
+        int flowerCount=0;
+
+        for(int f : arr){
+
+            if(f <= mid){
+                flowerCount++;
+            }else{
+                flowerCount=0;
+            }
+
+            if(flowerCount==flower){
+                bouquetCount++;
+                flowerCount=0;
+            }
+
+            if(bouquetCount >= bouquet)return true;
+
+
+        }
+        return false;
+        
+    }
+    public int getMax(int[] arr){
+        int max=0;
+        for(int b : arr){
+            max=Math.max(max,b);
+        }
+        return max;
+    }
+    public int getMin(int[] arr){
+        int min= Integer.MAX_VALUE;
+        for(int b : arr){
+            min=Math.min(min,b);
+        }
+        return min;
+    }
+}
