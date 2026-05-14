faba070d4fb8a61b149522d4937ac92ccc45aa23
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-05-14 16:03:00 +0800

    Time: 25 ms (32.66%), Space: 48.2 MB (50.91%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    public int calculate(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int num = 0;
+        char sign = '+';
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            // Build number
+            if(Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            // Operator OR end of string
+            if((!Character.isDigit(ch) && ch != ' ') ||
+                i == s.length() - 1) {
+
+                switch(sign) {
+
+                    case '+':
+                        stack.push(num);
+                        break;
+
+                    case '-':
+                        stack.push(-num);
+                        break;
+
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+
+                    case '/':
+                        stack.push(stack.pop() / num);
+                        break;
+                }
+
+                sign = ch;
+                num = 0;
+            }
+        }
+
+        int result = 0;
+
+        while(!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+}
