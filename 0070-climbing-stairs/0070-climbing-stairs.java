9479353fcef8f158db8da01d5be55883cd269eef
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-06-24 10:53:08 +0800

    Time: 0 ms (100%), Space: 41.8 MB (93.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int[] memo = new int[n+1];
+
+        return dp(n,memo);
+        
+    }
+    public int dp(int n, int[] memo){
+        if(n == 1 || n== 2)return n;
+
+        if(memo[n] != 0)return memo[n];
+
+        memo[n] = dp(n-1,memo) + dp(n-2,memo);
+
+        return memo[n];
+
+    }
+}
