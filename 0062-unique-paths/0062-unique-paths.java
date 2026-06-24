704e7218c1a8630b85d8e86a32b3395c4e4f84c6
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-06-24 15:30:16 +0800

    Time: 0 ms (100%), Space: 42 MB (65.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -23,3 +23,28 @@ class Solution {
         return dp[m - 1][n - 1];
     }
 }
+
+// class Solution {
+
+//     public int uniquePaths(int m, int n) {
+//         int[][] memo = new int[m][n];
+//         return dfs(m - 1, n - 1, memo);
+//     }
+
+//     private int dfs(int i, int j, int[][] memo) {
+
+//         // out of grid
+//         if (i < 0 || j < 0) return 0;
+
+//         // start cell
+//         if (i == 0 && j == 0) return 1;
+
+//         // already computed
+//         if (memo[i][j] != 0) return memo[i][j];
+
+//         // recursion
+//         memo[i][j] = dfs(i - 1, j, memo) + dfs(i, j - 1, memo);
+
+//         return memo[i][j];
+//     }
+// }
