6d3a0e4756daba8c602f44d72c3a3439d4568d83
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-06-24 10:44:40 +0800

    Time: 0 ms (100%), Space: 41.7 MB (92.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,22 +1,47 @@
 class Solution {
     public int fib(int n) {
-
-        
-        int[] memo = new int[n+1];
-        if(n == 0 || n ==1)return n;
-        memo[0] = 0;
-        memo[1] = 1;
-
-        return dp(n,memo);
-        
+        int[] memo = new int[n + 1];
+        return dp(n, memo);
     }
 
-    public int dp(int n, int[] memo){
-        if(n == 0 || n== 1)return n;
+    public int dp(int n, int[] memo) {
+        if (n == 0 || n == 1) return n;
 
-        memo[n] = dp(n-1,memo) + dp(n-2,memo);
+        if (memo[n] != 0) return memo[n];
 
+        memo[n] = dp(n - 1, memo) + dp(n - 2, memo);
         return memo[n];
-
     }
 }
+
+// class Solution {
+//     public int fib(int n) {
+//         if (n <= 1) return n;
+
+//         int[] dp = new int[n + 1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+
+//         for (int i = 2; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+
+//         return dp[n];
+//     }
+// }
+
+// class Solution {
+//     public int fib(int n) {
+//         if (n <= 1) return n;
+
+//         int a = 0, b = 1;
+
+//         for (int i = 2; i <= n; i++) {
+//             int c = a + b;
+//             a = b;
+//             b = c;
+//         }
+
+//         return b;
+//     }
+// }
