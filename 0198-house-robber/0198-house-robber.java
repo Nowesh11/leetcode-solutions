a7097f38ae4ff8073766717ac60261dc8aed1cdc
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-06-24 14:51:23 +0800

    Time: 0 ms (100%), Space: 42.9 MB (18.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,51 @@
-class Solution {
-    public int rob(int[] nums) {
+// class Solution {
+//     public int rob(int[] nums) {
 
+//         int n = nums.length;
+
+//         if(n < 2){
+//             return nums[0];
+//         }
+//         int[] dp = new int[n];
+//         dp[0] = nums[0];
+//         dp[1] = Math.max(nums[0],nums[1]);
+
+//         for(int i =2;i<n;i++){
+
+//             dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+//         }
+
+//         return dp[n-1];
+        
+//     }
+// }
+
+class Solution {
+
+    public int rob(int[] nums) {
         int n = nums.length;
 
-        if(n < 2){
-            return nums[0];
-        }
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-        for(int i =2;i<n;i++){
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
 
-            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
-        }
+        return dp(n - 1, nums, memo);
+    }
 
-        return dp[n-1];
-        
+    public int dp(int n, int[] nums, int[] memo) {
+
+        if (n == 0) return nums[0];
+        if (n == 1) return Math.max(nums[0], nums[1]);
+
+        if (memo[n] != -1) return memo[n];
+
+        memo[n] = Math.max(
+            dp(n - 2, nums, memo) + nums[n],
+            dp(n - 1, nums, memo)
+        );
+
+        return memo[n];
     }
 }
