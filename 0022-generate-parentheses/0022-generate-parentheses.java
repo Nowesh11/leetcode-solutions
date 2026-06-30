87b013678f7d727ec1e7124e68fda7d05a85e162
Nowesh11 <136292413+Nowesh11@users.noreply.github.com>
2026-06-30 16:14:01 +0800

    Time: 2 ms (71.88%), Space: 45.1 MB (9.6%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        if(n == 0)return res;
+
+        backtrack(n,0,0,"");
+
+        return res;
+        
+    }
+
+    private void backtrack(int n, int open, int close, String str){
+
+        if(str.length() == (2*n)){
+            res.add(str);
+            return;
+        }
+
+        if(open < n){
+            backtrack(n,open+1,close,str + "(");
+        }
+
+        if(close < open){
+            backtrack(n,open,close+1,str + ")");
+        }
+    }
+}
